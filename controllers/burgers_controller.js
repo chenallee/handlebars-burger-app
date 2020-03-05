a83ae75432a2d1beb23d5c08d02f8761ff03377f
chenallee <lchenalloy@gmail.com>
2020-03-05 12:04:45 -0500

    api routes added

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,79 @@
+const connection = require('../config/connection');
+
+// get all burgers
+const getBurgers = () => {
+  return new Promise((resolve, reject) => {
+    connection.query('SELECT * FROM burgers', (err, burgerdata) => {
+      if (err) {
+        console.log(err);
+        //goes to promise's .catch():
+        return reject(err);
+      }
+      //goes to promise's .then():
+      resolve(burgerdata);
+    });
+  });
+}
+
+// add new burger
+/* --------------------------------
+  accepts object parameter => 
+  {burger_name: 'Feta Turkey Burger'}
+----------------------------------- */
+const createBurger = burgerObj => {
+  return new Promise((resolve, reject) => {
+    connection.query('INSERT INTO burgers SET ?', burgerObj, (err, burgerdata) => {
+      if (err) {
+        console.log(err);
+        //goes to promise's .catch():
+        return reject(err);
+      }
+      //goes to promise's .then():
+      resolve(burgerdata);
+    });
+  });
+}
+
+// update burger's devoured status
+/* --------------------------------
+  burgerObj => 
+  {devoured: true} OR {devoured: false}
+----------------------------------- */
+const updateBurger = (burgerObj, burgerId) => {
+  return new Promise ((resolve, reject) => {
+    connection.query('UPDATE burgers SET ? WHERE id = ?', [burgerObj, burgerId], (err, burgerdata) => {
+      if (err) {
+        console.log(err);
+        //goes to promise's .catch():
+        return reject(err);
+      } else if (burgerdata.affectedRows === 0){ // if query connects but nothing to update
+        //goes to promise's .then():
+        return resolve({ message: `Couldn't find a burger with that id!`, code: 404 });
+      }
+      //goes to promise's .then():
+      resolve({ messahe: 'Burger updated!', code: 200});
+    });
+  });
+}
+
+// delete burger
+const deleteBurger = burgerId => {
+  return new Promise ((resolve, reject) => {
+    connection.query('DELETE FROM burgers WHERE id = ?', [burgerId], (err, burgerdata) => {
+      if (err) {
+        console.log(err);
+        //goes to promise's .catch():
+        return reject(err);
+      } else if (burgerdata.affectedRows === 0){ // if query connects but nothing to delete
+        //goes to promise's .then():
+        return resolve({ message: `Couldn't find a burger with that id!`, code: 404 });
+      }
+      //goes to promise's .then():
+      resolve({ messahe: 'Burger deleted!', code: 200});
+    });
+  });
+}
+
+module.exports = {
+  getBurgers, createBurger, updateBurger, deleteBurger
+}
---
+++ routes/api/burger_routes.js
@@ -0,0 +1,58 @@
+const router = require('express').Router();
+
+const { getBurgers, createBurger, updateBurger, deleteBurger } = require('../../controllers/burgers_controller');
+
+//create full CRUD routes at `/burgers` (which is actually `/api/burgers`... see index.js)
+
+/* GET => Get All  Burgers */
+router.get('/burgers', (req, res) => {
+  getBurgers()
+    .then(burgerdata => {
+      res.status(200).json(burgerdata);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+/* POST => Add Burger */
+router.post('/burgers', (req, res) => {
+  //req.body => {burger_name: 'Mushroom Swiss Burger'}
+  createBurger(req.body)
+    .then(burgerdata => {
+      res.status(200).json(burgerdata);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+/* PUT => Update Burger: devoured: true OR false */
+router.put('/burgers/:id', (req, res) => {
+  updateBurger(req.body, req.params.id)
+    .then(burgerdata => {
+      if (burgerdata.code === 404) { //if query connected but nothing updated
+        return res.status(404).json(burgerdata); //return 404 and escape
+      }
+      res.status(200).json(burgerdata);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+/* DELETE => Delete Burger */
+router.delete('burgers/:id', (req, res) => {
+  deleteBurger(req.params.id)
+    .then(burgerdata => {
+      if (burgerdata.code === 404){ //if query connected but nothing deleted
+        return res.status(404).json(burgerdata); //return 404 and escape
+      }
+      res.status(200).json(burgerdata);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+module.exports = router;
